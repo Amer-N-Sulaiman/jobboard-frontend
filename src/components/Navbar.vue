ac926238a18a465f12c35231fd9c3a57f4da925d
Amer-N-Sulaiman <amersulaiman10@gmail.com>
2024-04-20 13:30:51 +0300

    New simpler navbar.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,13 @@
 <template>
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+    <!-- <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
         <div class="container-fluid">
-            <!-- Title -->
             <router-link to="/" class="navbar-brand" href="#">ANS Job Board</router-link>
 
-            <!-- Toggle Button -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <!-- Logged Out Menu -->
+            
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
@@ -33,7 +31,24 @@
                 </ul>
             </div>
 
-            <!-- Logged In Menu -->
+        </div>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <router-link to="/" class="navbar-brand" href="#">ANS Job Board</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <router-link class="nav-link active" aria-current="page" to="/listings">Jobs Listings</router-link>
+                <router-link v-if="!token" class="nav-link" to="/signup">Signup</router-link>
+                <router-link v-if="!token" class="nav-link" to="/login">Login</router-link>
+                <router-link v-if="token" class="nav-link" to="/account" >Account</router-link>
+                <router-link v-if="token" class="nav-link" to="/addjob" >Post a Job</router-link>
+                <router-link v-if="token" class="nav-link" to="#" @click="handleLogout">Logout</router-link>
+            </div>
+            </div>
         </div>
     </nav>
 </template>
